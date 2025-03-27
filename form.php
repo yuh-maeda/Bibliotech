9a4b3c8d279db81bd38352ed6d3bb6b00e3d16b6
yuh-maeda <yurimaedanick019@gmail.com>
2025-03-27 16:55:00 -0400

    ligação com banco de dados

---
+++ form.php
@@ -0,0 +1,79 @@
+<?php
+    include_once("conexao.php");
+
+    if(isset($_POST['adicionar'])){
+        $titulo = $_POST['titulo'];
+        $autor = $_POST['autor'];
+        $data_pub = $_POST['data_pub'];
+        $data_cheg = $_POST['data_cheg'];
+        $sinopse = $_POST['sinopse'];
+        $genero = $_POST['genero'];
+    
+        $sql_update = "INSERT INTO livro (titulo, autor, dataPub,dataCheg,genero,sinopse) VALUES ('$titulo','$autor', '$data_pub','$data_cheg','$sinopse','$genero')";
+    
+        $result_update = mysqli_query($conn,$sql_update);
+        
+        if($result_update){
+            header('location:index.php');
+        }
+        else{
+            echo'<script>alert("Não foi possível cadastrar o livro!")</script>';
+            print_r($_POST);
+        }
+    
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bibliotech</title>
+    <link rel="stylesheet" href="form.css">
+    <link rel="shortcut icon" href="livrinho.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <form class="form-livros" action="" method="post">
+        <div class="area-input">
+            <h1 class="form-h1">Titulo</h1>
+            <input type="text" name="titulo" class="form-input">
+        </div>
+
+        <div class="area-input">
+            <h1 class="form-h1">Autor</h1>
+            <input type="text" name="autor" class="form-input">
+        </div>
+
+        <div class="area-input">
+            <h1 class="form-h1">Data de Publicação</h1>
+            <input type="date" name="data_pub" class="form-input">
+        </div>
+
+        <div class="area-input">
+            <h1 class="form-h1">Data de chegada</h1>
+            <input type="date" name="data_cheg" class="form-input">
+        </div>
+
+        <div class="area-input genero">
+            <h1 class="form-h1">Gênero</h1>
+            <input type="text" name="genero" class="form-input">
+        </div>
+
+        <div class="area-input area-sinopse">
+            <h1 class="form-h1">Sinopse</h1>
+            <textarea name="sinopse"  class="text-sinopse"></textarea>
+        </div>
+        
+        <input type="submit" name="adicionar" value="Adicionar" class="btn submit">
+
+    </form>
+
+    <div class="area-btn-voltar">
+        <a href="index.php" class="btn-voltar"><i class="fa-solid fa-arrow-left"></i></a>
+    </div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
     include_once("conexao.php");
+    $sql = 'SELECT * FROM livro';
+    $result = mysqli_query($conn,$sql);
+
 ?>
 
 <!DOCTYPE html>
@@ -28,27 +31,67 @@
         <div class="area-titulo-btn">
             <h1 class='titulo'>Todos os Livros</h1>
             <div class="area-btns">
-                <a href="form.html" class="btn btn-add">Adicionar Livros</a>
+                <a href="form.php" class="btn btn-add">Adicionar Livros</a>
             </div>
         </div>
 
         <div class="livros">
             
             <div class="card-info">
+
+                <?php
+                    if ($result){
+                        while($row = mysqli_fetch_assoc($result)){
+                            $id = $row['id'];
+                            $titulo = $row['titulo'];
+                            $sinopse = $row['sinopse'];
+                            $genero = $row['genero'];
+                            $autor = $row['autor'];
+                            $data_pub = $row['dataPub'];
+                            $data_cheg = $row['dataCheg'];
+
+                            echo"<div class='card-info'>
+ 
+                                <div class='area-tiutlo-id'>
+                                <h1 class='livro-id'>$id</h1>
+                                <h1 class='livro-titulo'>$titulo</h1>
+                                <p class='livro-sinopse'>$sinopse</p>
+                                </div>
+                                
+                                <p class='dado livro-genero'>Genero: $genero</p>
+                                <p class='dado livro-autor'>Autor: $autor</p>
+                                
+                                <div class='area-datas'>
+                                    <p class='dado livro-data'>Data de Publicação: $data_pub</p>
+                                    <p class='dado livro-data'>Data de chegada: $data_cheg</p>
+                                </div> 
+                            </div>";
+                        }
+                    }else{
+                        echo"Não á livros";
+                    }
+                ?>
+
+            </div>
+
+
+
+        <!-- <div class="card-info">
                 <div class="area-tiutlo-id">
                     <h1 class="livro-id">1</h1>
                     <h1 class="livro-titulo">Livro do Machado</h1>
                     <p class="livro-sinpse">Um piloto perdido no deserto encontra um jovem príncipe que veio de um pequeno planeta. Juntos, eles compartilham conversas sobre a vida, o amor e a natureza humana, com o príncipe contando suas experiências em diferentes planetas. A história é uma reflexão poética sobre a infância, os sentimentos e a busca por sentido no mundo adulto.</p>
                 </div>
                 
+                <p class="dado livro-genero">Genero: História</p>
                 <p class="dado livro-autor">Autor: Machado de Asis</p>
-
+                
                 <div class="area-datas">
                     <p class="dado livro-data">Data de Publicação: 25/25/25</p>
                     <p class="dado livro-data">Data de chegada: 25/25/25</p>
                 </div>
 
-            </div>
+            </div> -->
         
         </div>
 
